4921b56df4bcc053e8ca7ab4d5e49cf3c1a4d514
g0lemn <abdusalam.solemkkhan@gmail.com>
2018-08-11 19:14:19 -0700

    Initial Commit

---
+++ Angular Exercise 1/app/app.js
@@ -0,0 +1,69 @@
+var angularApp = angular.module('angularExercise1', ['ngRoute']);
+
+// this will fire before the application runs
+angularApp.config(['$routeProvider', function($routeProvider){
+  $routeProvider
+    .when('/home', {
+      templateUrl: 'views/home.html',
+      controller: 'DevController'
+    })
+    .when('/directory', {
+      templateUrl: 'views/directory.html',
+      controller: 'DevController'
+    }).otherwise({
+      redirectTo: '/home'
+    });
+}]);
+
+// this will fire upon application runs
+// angularApp.run(function(){});
+
+// Creating directives or simple your own function/method
+// ways to restrict directives in angular
+// 1. E = elements
+// 2. A = attributes
+angularApp.directive('randomDeveloper', [function(){
+  return {
+    restrict: 'E', // sets what kind of what your directive affects.
+    scope: {       // key value pair for what scope this directive could only utilize
+      devs: '=',   // declared in the as part of the elemet random-developer / randomDeveloper
+      title: '='   // declared in the as part of the elemet random-developer / randomDeveloper
+    },
+    templateUrl: 'views/random.html',
+    transclude: true,
+    replace: true,
+    controller: function($scope){
+      $scope.random = Math.floor(Math.random() * 4);
+    }
+  };
+}]);
+
+
+// will handle funtions
+angularApp.controller("DevController", ['$scope', '$http', function($scope, $http) {
+
+  $scope.newDevPeep = function() {
+    $scope.devPeeps.push({
+      name: $scope.newDev.DevName,
+      rank: $scope.newDev.DevRank,
+      years: parseInt($scope.newDev.DevYears),
+      color: "gray",
+      is_present: true
+    });
+
+    $scope.newDev.DevName = "";
+    $scope.newDev.DevRank = "";
+    $scope.newDev.DevYears = "";
+
+  };
+
+  $scope.resignDevPeep = function(dev) {
+    var devPeep = $scope.devPeeps.indexOf(dev);
+    $scope.devPeeps.splice(devPeep, 1);
+  };
+
+  $http({method: 'get', url: 'data/devPeeps.json'}).then(function(response){
+    $scope.devPeeps = response.data;
+  });
+
+}]);
